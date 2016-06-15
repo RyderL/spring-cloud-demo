a09fb406c3e4ee16a007d1f5466e1c071a87c363
chenzhenjia <chzhenjia@gmail.com>
2016-06-15 13:49:10 +0800

    add: config-server

---
+++ config-server/src/main/java/com/ai5suoai/cloud/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.ai5suoai.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * spring cloud 的配置服务器
+ *
+ * @author chenzhenjia
+ * @since 16/6/15
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
